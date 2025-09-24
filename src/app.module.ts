767ee1ff9c64033009056d98c765fb5e1c6debd0
FloresAlvaro <alvarofloresfarfan2802@gmail.com>
2025-09-23 21:55:10 -0400

    feat: add grades, roles, and subjects modules with controllers and services
    - Implemented Grades module with CRUD operations
    - Implemented Roles module with CRUD operations
    - Implemented Subjects module with CRUD operations
    - Added DTOs for create and update operations in each module
    - Created entity classes for Grades, Roles, and Subjects
    - Added unit tests for controllers and services in each module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RoleModule } from './common/role/role.module';
+import { RolesModule } from './common/roles/roles.module';
+import { GradesModule } from './common/grades/grades.module';
+import { SubjectsModule } from './common/subjects/subjects.module';
 
 @Module({
-  imports: [],
+  imports: [RoleModule, RolesModule, GradesModule, SubjectsModule],
   controllers: [AppController],
   providers: [AppService],
 })
